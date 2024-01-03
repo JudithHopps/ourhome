8b348fd3b19443675da8724fe0a70a4b401b3041
JudithHopps <kookjeayon100@gmail.com>
2024-01-03 18:18:53 +0900

    feat : button Component 구현

---
+++ component/common/Button.tsx
@@ -0,0 +1,60 @@
+import React, { ReactNode, CSSProperties } from "react";
+import styled from "styled-components";
+
+interface ButtonComponentProps {
+  children: ReactNode;
+  importance?: "high" | "medium" | "low";
+  width?: number;
+  height?: number;
+  onClick?: () => void;
+}
+
+const getButtonColor = (importance?: "high" | "medium" | "low"): string => {
+  switch (importance) {
+    case "high":
+      return "#ff6500;";
+    case "medium":
+      return "#ff9c00;";
+    case "low":
+      return "#aaa";
+    default:
+      return "white";
+  }
+};
+
+const S = {
+  ButtonComponent: styled.div<ButtonComponentProps>`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center; /* Center the text vertically */
+    padding: 10px 20px;
+    background-color: ${(props) => getButtonColor(props.importance)};
+    ${(props) => props.width && `width: ${props.width}px;`}
+    ${(props) => props.height && `height: ${props.height}px;`}
+    color: white;
+    text-align: center;
+    cursor: pointer;
+  `,
+};
+
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  children,
+  importance,
+  width,
+  height,
+  onClick,
+}) => {
+  return (
+    <S.ButtonComponent
+      importance={importance}
+      width={width}
+      height={height}
+      onClick={onClick}
+    >
+      {children}
+    </S.ButtonComponent>
+  );
+};
+
+export default ButtonComponent;
